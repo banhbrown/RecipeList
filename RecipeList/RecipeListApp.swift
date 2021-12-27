fab50c1fc0ee4267647be4f75b47591c1c9ae961
banhbrown <91293862+banhbrown@users.noreply.github.com>
2021-12-26 22:01:53 -0600

    Initial Commit

---
+++ RecipeList/RecipeListApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeListApp.swift
+//  RecipeList
+//
+//  Created by Brianna Brown on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct RecipeListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
